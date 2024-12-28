dc209a3d73842aaaebdaa9d61102bf651738f671
Mitchell Henke <mitchell@mitchellhenke.com>
2024-12-28 14:06:27 -0600

    live updates

--- config/prod.exs
+++ config/prod.exs
@@ -7,6 +7,8 @@ import Config
 # before starting your production server.
 config :civics, CivicsWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
+config :civics, download_and_seed: true
+
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Civics.Finch
 
